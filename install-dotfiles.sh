e97446a4bbef4a1fbdbd627b69072000f76987fa
HeadlessChild <dazethelaze@gmail.com>
2015-08-26 15:45:29 +0200

    fixing things..

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -9,14 +9,14 @@ cp -v scripts/i3lock.sh ~/bin
 cp -v wallpapers/icon.png ~/.xlock
 cp -v i3/config ~/.i3
 cp -v i3/i3blocks.conf ~/.i3blocks.conf
-cp -v misc/Xdefauls ~/.Xdefaults
+cp -v misc/Xdefaults ~/.Xdefaults
 cp -v misc/zshrc ~/.zshrc
 cp -v misc/vimrc ~/.vimrc
 #################################################
 
 ############## Installing packages ##############
 echo "Installing packages (vim, i3-gaps deps, urxvt, font-awesome, i3-utils)..."
-sudo apt-get install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev pkg-config i3blocks i3lock ruby-ronn fonts-font-awesome rxvt-unicode-256color vim scrot zsh
+sudo apt-get install -y libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev pkg-config i3blocks i3lock ruby-ronn fonts-font-awesome rxvt-unicode-256color vim scrot zsh curl xclip suckless-tools
 #################################################
 
 ######## Compiling and installing i3-gaps ########
@@ -34,7 +34,10 @@ cd -
 
 ############ Fixing urxvt copy/paste ############
 echo "Fixing copy/paste for urxvt..."
-curl https://dl.dropboxusercontent.com/u/87084722/urxvtclip.tar.gz -o /usr/lib/urxvt/perl/clipboard.tar.gz | tar zx
+cd /usr/lib/urxvt/perl
+sudo curl https://dl.dropboxusercontent.com/u/87084722/urxvtclip.tar.gz | sudo tar xz
+sudo mv urxvtclip clipboard
+cd -
 #################################################
 
 # Installing vim pathogen and installing gruvbox #
@@ -47,20 +50,27 @@ git clone https://github.com/morhetz/gruvbox.git ~/.vim/bundle/gruvbox
 
 ############ Installing Google Chrome ############
 echo "Adding Google Chrome repository..."
-echo "deb http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/source.list.d/google-chrome.list
+echo "deb http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
 echo "Adding Google Chrome repository signing key..."
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 echo "Updating deb repositories and installing google-chrome-stable..."
-sudo apt-get update && sudo apt-get install google-chrome-stable
+sudo apt-get update && sudo apt-get install -y google-chrome-stable
 ###################################################
 
 ############### Installing Spotify ################
 echo "Adding Spotify repository..."
-echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/source.list.d/spotify.list
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 echo "Adding Spotify repository signing key..."
-sudo apt-key add --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys D2C19886
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys D2C19886
 echo "Updating deb repositories and installing spotify-client..."
-sudo apt-get update && sudo apt-get install spotify-client
+sudo apt-get update && sudo apt-get install -y spotify-client
+###################################################
+
+############ Installing dmenu_extended ############
+wget https://github.com/markjones112358/dmenu-extended/archive/master.zip
+unzip master.zip
+sudo python dmenu-extended-master/setup.py install
+sudo rm -rf dmenu-extended-master master.zip
 ###################################################
 
 #################### Oh My Zsh ###################
