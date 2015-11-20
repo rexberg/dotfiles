15580af3c7af3569a5ef3b54e56329c0c147da17
HeadlessChild <dazethelaze@gmail.com>
2015-11-20 13:56:43 +0100

    fixing test script

--- .test.sh
+++ .test.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-set -x
-set -e
-
-for i in $(find . -type f -executable -not -path "./.git/*"); do 
-    shellcheck $i
+for i in $(find . -type f -executable -not -path "./.git/*" ! -name "*.test*" ! -name "*install-dotfiles_ubuntu-trusy_tahr.sh.OUT_OF_DATE*"); do 
+    shellcheck "$i"
+    if [ "$?" -ge 1 ]; then
+	echo -e "---\nFailed! $i\n---"
+    else
+	echo -e "---\nSucessfully linted! $i\n---"
+    fi
 done
