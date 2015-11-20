9c64078976dfe340ae2e090690cc90d13f4c7d20
HeadlessChild <dazethelaze@gmail.com>
2015-11-20 08:23:29 +0100

    adding bash debug instead of echo

--- .test.sh
+++ .test.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
+set -x
 set -e
 
-(
 for i in $(find . -type f -executable -not -path "./.git/*"); do 
-    shellcheck $i && echo -e "---\nSucessfully linted $i\n---"
+    shellcheck $i
 done
-)||true
