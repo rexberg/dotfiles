373b89f555f920fd2a1796b186ad64e9ec15074f
HeadlessChild <dazethelaze@gmail.com>
2015-11-20 22:39:27 +0100

    better output

--- .test.sh
+++ .test.sh
@@ -3,8 +3,8 @@
 for i in $(find . -type f -executable -not -path "./.git/*" ! -name "*.test*" ! -name "*install-dotfiles_ubuntu-trusy_tahr.sh.OUT_OF_DATE*"); do 
     shellcheck "$i"
     if [ "$?" -ge 1 ]; then
-	echo -e "---\nFailed! $i\n---"
+		echo -e "---\nFailed! $i\n---"
     else
-	echo -e "---\nSucessfully linted! $i\n---"
+		echo -e "---\nSucessfully linted $i\n---"
     fi
 done
