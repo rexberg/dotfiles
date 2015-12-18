a0240aeca13d04640a617ac40962e9dcebc02bbc
HeadlessChild <dazethelaze@gmail.com>
2015-12-18 14:46:16 +0100

    preventing globbing and word splitting

--- install-dotfiles_debian-stretch-noX11-unattended.sh
+++ install-dotfiles_debian-stretch-noX11-unattended.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-home_user="$USER"
+local_user="$USER"
 
 ######## Creating dirs and moving files ########
 echo "Creating directories and moving files..."
-cp -v misc/zshrc /home/$home_user/.zshrc
-cp -v misc/vimrc /home/$home_user/.vimrc
+cp -v misc/zshrc /home/"$local_user"/.zshrc
+cp -v misc/vimrc /home/"$local_user"/.vimrc
 #################################################
 
 ############## Installing packages ##############
@@ -15,14 +15,14 @@ sudo apt-get install -y pkg-config vim zsh curl weechat unzip
 
 ######## Installing pathogen and plugins ########
 echo "Installing pathogen..."
-mkdir -pv /home/$home_user/.vim/autoload /home/$home_user/.vim/bundle
-curl -LSso /home/$home_user/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+mkdir -pv /home/"$local_user"/.vim/autoload /home/"$local_user"/.vim/bundle
+curl -LSso /home/"$local_user"/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 echo "Installing colorscheme gruvbox for vim..."
-git clone https://github.com/morhetz/gruvbox.git /home/$home_user/.vim/bundle/gruvbox
+git clone https://github.com/morhetz/gruvbox.git /home/"$local_user"/.vim/bundle/gruvbox
 echo "Installing syntastic..."
-git clone https://github.com/scrooloose/syntastic /home/$home_user/.vim/bundle/syntastic
+git clone https://github.com/scrooloose/syntastic /home/"$local_user"/.vim/bundle/syntastic
 echo "Installing vim-airline..."
-git clone https://github.com/bling/vim-airline /home/$home_user/.vim/bundle/vim-airline
+git clone https://github.com/bling/vim-airline /home/"$local_user"/.vim/bundle/vim-airline
 ##################################################
 
 ##### Installing pip (python package manager) #####
@@ -36,13 +36,13 @@ rm -fv get-pip.py
 echo "Installing rawinbowstream..."
 sudo pip install rainbowstream
 #fix for rainbowstream crash, cause of the use of sudo
-sudo cp /root/.rainbow_config.json /home/$home_user/
-sudo chown markus:markus /home/$home_user/.rainbow_config.json
+sudo cp /root/.rainbow_config.json /home/"$local_user"/
+sudo chown markus:markus /home/"$local_user"/.rainbow_config.json
 ###################################################
 
 #################### Oh My Zsh ###################
 echo "Installing oh-my-zsh..."
-git clone https://github.com/robbyrussell/oh-my-zsh.git /home/$home_user/.oh-my-zsh
-cp -v /home/$home_user/dot-files/misc/headless.zsh-theme /home/$home_user/.oh-my-zsh/themes
+git clone https://github.com/robbyrussell/oh-my-zsh.git /home/"$local_user"/.oh-my-zsh
+cp -v /home/"$local_user"/dot-files/misc/headless.zsh-theme /home/"$local_user"/.oh-my-zsh/themes
 #chsh -s /bin/zsh markus #not working
 ##################################################
