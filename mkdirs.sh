233a530610b15606c81fbf601eb60c876dbd3c26
rexberg <m@rexhepi-lindberg.com>
2024-04-19 10:38:33 +0200

    {} does not work with sh.

--- mkdirs.sh
+++ mkdirs.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-mkdir -vp ~/.local/{bin,lib}
-mkdir -vp ~/.local/share/{applications,icons,man}
-mkdir -vp ~/.local/var/log
+mkdir -vp ~/.local/bin
+mkdir -vp ~/.local/lib
+mkdir -vp ~/.local/share/applications
 mkdir -vp ~/.local/share/bash-completion/completions
+mkdir -vp ~/.local/share/icons
+mkdir -vp ~/.local/share/man
+mkdir -vp ~/.local/var/log
